09120c4b560b05e2fe854c06ae7703b0d45d4738
EmilyUry <ury.emily@gmail.com>
2021-09-14 09:48:38 -0400

    cleaned up and commented code

--- NWI_download.R
+++ NWI_download.R
@@ -16,42 +16,37 @@
 
 setwd("C:/Users/Emily Ury/OneDrive - University of Waterloo/Wetlands_local/Data_files/NWI_Wetlands_US")
 
+#' Define a region of interest (there are 18 in the conterminus US)
+ROI <- "05"  
 
-ROI <- "09"  # define a region of interest, there are 18 (19-21 are AK, HI and PR)
 
+#' Calls in an input file, the INDEX, which is a list of the HUC numbers in
+#' Region of interest
 DataWD <- "C:/Users/Emily Ury/OneDrive - University of Waterloo/Wetlands_local/Data_files/NWI_Wetlands_US/"
 INDEX <- read.csv(paste(DataWD, 'Region_', ROI, '_key.csv', sep = ""), header = F)
 INDEX <- INDEX[,1] # now index is a list instead of a dataframe
 
 
-## this calls in an input file, the INDEX, which is a list of the HUC numbers in
-## the Region of Interest
-
-## this works for the first watershed in the INDEX
-
-HUC <- INDEX[1,1]
-
-download.file(paste('https://www.fws.gov/wetlands/downloads/Watershed/HU8_0',
-                    HUC, '_watershed.zip', sep = ""), 
-              paste(DataWD, 'Region_09/HU8_0', HUC, '_watershed.zip', sep = ""))
-
-
-
+#' Write a function to download the file of interest
+#' NOTE: for regions 10-18, you need to delete the leading zero in lines 33 and 35
 Download_Wetlands <- function(INDEX) {
   download.file(paste('https://www.fws.gov/wetlands/downloads/Watershed/HU8_0',
                       INDEX, '_watershed.zip', sep = ""), 
-                paste(DataWD, 'Region_09/HU8_0', INDEX, '_watershed.zip', sep = ""))
+                paste(DataWD, 'Region_', ROI,  '/HU8_0', INDEX, '_watershed.zip', sep = "")) 
 }
 
 
-## run the function and note how long it takes to download 'n' watersheds
+#' run the function, over the entire INDEX list and 
+#' note how long it takes to download 'n' watersheds
 start_time <- Sys.time()
 
-#Download_Wetlands(INDEX)  # this works for first in list INDEX
 lapply(INDEX, Download_Wetlands)
 
 end_time <- Sys.time()
 end_time - start_time
+### takes about 0.5 minutes * INDEX length
+
+
 
 
 
