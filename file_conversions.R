359d4356adb69642da3bd0198eee0891383dda80
EmilyUry <ury.emily@gmail.com>
2021-10-21 21:13:03 -0400

    list to string

--- file_conversions.R
+++ file_conversions.R
@@ -12,3 +12,23 @@ d <- read_sf("Soil_Survey_Complex.geojson")
 
 
 st_write(d, "CAN_soil.shp")
+
+
+
+### convert a list to a string
+
+setwd("C:/Users/Emily Ury/Desktop/New folder")
+
+x <- read.csv("soilCode.csv")
+list <- x[,1]
+
+
+string <- paste(shQuote(list, type = 'sh'), collapse =', ')
+string
+
+
+
+x <- read.csv("symbol1.csv")
+list <- x[,1]
+string <- paste(shQuote(list, type = 'sh'), collapse =', ')
+string
