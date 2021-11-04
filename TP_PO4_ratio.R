be94f982a4e1e0401cf6ba7fff1b6e946f3464d0
EmilyUry <ury.emily@gmail.com>
2021-11-04 10:30:23 -0400

    remade P retention / speciation plots

--- TP_PO4_ratio.R
+++ TP_PO4_ratio.R
@@ -22,6 +22,8 @@ setwd("C:/Users/Emily Ury/OneDrive - University of Waterloo/Wetlands_local/Data_
 
 
 library(viridis)
+library(dplyr)
+library(tidyr)
 
 
 ## Fred's meta-analysis of literature values
@@ -29,8 +31,8 @@ x <- read.csv("P_speciation2.csv", header = T)
 
 
 ## bin data by wetland size into 5 roughly evenly sized bins
-breaks  <- c(0,30, 2000, 15000, 500000, 10000000000)
-tags <- c("0-30","30-2k", "2k-15k", "15k-500k", "500k+")
+breaks  <- c(0,30, 3000, 15000, 100000, 10000000000)
+tags <- c("0-30","30-3k", "3k-15k", "15k-100k", "100k+")
 group_tags <- cut(x$area,
                   breaks = breaks,
                   include.lowest = TRUE, right = FALSE, 
@@ -58,8 +60,6 @@ boxplot(PO4_Retention ~ area_bins, data = x, ylab = "fraction retained",
         xlab = "Wetland area (m2)", main = "PO4")
 
 
-
-
 ### magnification factor = PO4/TP
 
 x$mag.f <- x$PO4_Retention/x$TP_Retention
@@ -67,26 +67,106 @@ x <- x[which(x$mag.f < 15),]
 boxplot(mag.f ~ area_bins, data = x, ylab = "PO4 retention / TP retention",
         xlab = "Wetland area (m2)", main = "Magnification factor")
 
-
-
-
+x$mag.f.norm <- x$mag.f/x$TP_in
+boxplot(mag.f.norm ~ area_bins, data = x, ylab = "PO4 retention / TP retention / TP(in)",
+        xlab = "Wetland area (m2)", main = "Magnification factor, normalized by TP(inflow)",
+        ylim = c(-50,150))
 
 ## summary figure
 
+# tiff(filename = "P Retention.tiff", height=9600, width=4800, units= "px", res=800, compression= "lzw")
+# 
+# {
+# layout(matrix(1:3, ncol = 1))
+# par(oma = c(3,3,1,4), mar =c(4,4,1,1))
+# boxplot(TP_Retention ~ area_bins, data = x, ylab = "fraction retained",
+#         xlab = "", main = "TP")
+# boxplot(PO4_Retention ~ area_bins, data = x, ylab = "fraction retained",
+#         xlab = "", main = "PO4")
+# boxplot(mag.f ~ area_bins, data = x, ylab = "PO4 retention / TP retention",
+#         xlab = "Wetland area (m2)", main = "Magnification factor")
+# }
+# dev.off()
 
-tiff(filename = "P Retention.tiff", height=9600, width=4800, units= "px", res=800, compression= "lzw")
+
+## just surface water wetlands
+
+SFW <- x[which(x$Type == "CWSF"),]
+boxplot(TP_Retention ~ area_bins, data = SFW, ylab = "fraction retained",
+        xlab = "", main = "TP")
+boxplot(PO4_Retention ~ area_bins, data = SFW, ylab = "fraction retained",
+        xlab = "", main = "PO4")
+boxplot(mag.f ~ area_bins, data = SFW, ylab = "PO4 retention / TP retention",
+        xlab = "Wetland area (m2)", main = "Magnification factor")
+summary(SFW$area_bins)
+
+
+
+
+## convert the data to long, so we can box plot TP and PO4 together
+
+newd <- select(SFW, c(study, Depth, area, TP_Retention, PO4_Retention, area_bins))
+  
+mewd <- pivot_longer(newd, c(TP_Retention, PO4_Retention), names_to = "Species")  
+mewd$Species <- factor(mewd$Species, levels = c("TP_Retention", "PO4_Retention"), ordered = TRUE)
+
+
+tiff(filename = "P Retention.tiff", height=9600, width=5600, units= "px", res=800, compression= "lzw")
 
 {
-layout(matrix(1:3, ncol = 1))
-par(oma = c(3,3,1,4), mar =c(4,4,1,1))
-boxplot(TP_Retention ~ area_bins, data = x, ylab = "fraction retained",
-        xlab = "", main = "TP")
-boxplot(PO4_Retention ~ area_bins, data = x, ylab = "fraction retained",
-        xlab = "", main = "PO4")
-boxplot(mag.f ~ area_bins, data = x, ylab = "PO4 retention / TP retention",
-        xlab = "Wetland area (m2)", main = "Magnification factor")
+  layout(matrix(1:3, ncol = 1))
+  par(oma = c(3,3,2,4), mar =c(4,4,2,1))
+  boxplot(TP_Retention ~ area_bins, data = SFW, ylab = "fraction retained",
+          xlab = "", main = "TP")
+  boxplot(PO4_Retention ~ area_bins, data = SFW, ylab = "fraction retained",
+          xlab = "", main = "PO4")
+  boxplot(value ~ Species*area_bins, data = mewd, col = c("gray50", "white"), ylab = "fraction retained",
+          xlab = "", main = "Fraction P Retention", xaxt = "n", 
+          at = c(1,2,5,6,9,10,13,14,17,18))
+  axis(1, at = c(1.5, 5.5, 9.5, 13.5, 17.5), 
+       labels = tags)
+  legend("bottomleft", c("TP", "PO4"), pch = 22, pt.cex = 2, pt.bg = c("gray50", "white"))
 }
 dev.off()
+
+
+tiff(filename = "P Retention2.tiff", height=7200, width=5600, units= "px", res=800, compression= "lzw") 
+{
+  layout(matrix(1:2, ncol = 1))
+  par(oma = c(2,3,1,4), mar =c(4,4,4,1))
+  boxplot(mag.f ~ area_bins, data = SFW, ylab = "PO4 retention / TP retention",
+          xlab = "Wetland area (m2)", main = "Magnification factor")
+  
+  x$mag.f.norm <- x$mag.f/x$TP_in
+  boxplot(mag.f.norm ~ area_bins, data = SFW, ylab = "PO4 retention / TP retention / TP(in)",
+          xlab = "Wetland area (m2)", main = "Magnification factor, normalized by TP(inflow)",
+          ylim = c(-50,150)) 
+}
+dev.off()  
+
+
+
+
+
+
+
+
+
+
+  
+  
+summary(x$Depth)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
