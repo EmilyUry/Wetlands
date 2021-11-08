4ebe42addfe09ead05ebcd12c2bde255318080e3
EmilyUry <ury.emily@gmail.com>
2021-11-08 18:49:44 -0500

    K for PO4

--- K-Tau Phos.R
+++ K-Tau Phos.R
@@ -29,9 +29,11 @@ x <- read.csv("P_speciation2.csv", header = T)
 x <- x[which(x$area > 0),]
 x <- x[which(x$area < 1000000000),]
 
+table(x$Type)
+
 ## Tau = a(SA)^b
 
-plot(x$area, x$tau, log ="xy", pch = 16, col = c("darkgreen", "darkgreen", "darkgreen", "blue", "red")[x$Type])
+plot(log(x$area), log(x$tau), pch = 16, col = c("darkgreen", "darkgreen", "darkgreen", "blue", "red")[x$Type])
 legend("topleft", c("Wetland", "Lake", "Reservoir"), pch = 16, col = c("darkgreen", "blue", "red"))
 fit <- lm(log(tau)~log(area), data = x)
 abline(fit)
@@ -39,10 +41,22 @@ summary(fit)
 
 
 
-## calculate k (rate constant)
+## calculate k (rate constant) TP
 x$k2 <- -1*(log(1-x$TP_Retention)/x$tau)
-plot(x$tau, x$k2, log = "xy")
+plot(log(x$tau), log(x$k2), pch = 16, col = c("darkgreen", "darkgreen", "darkgreen", "blue", "red")[x$Type])
+legend("topright", c("Wetland", "Lake", "Reservoir"), pch = 16, col = c("darkgreen", "blue", "red"))
+fit <- lm(log(k2)~log(tau), data = x)
+abline(fit)
+summary(fit)
 
 
-table(x$Type)
- 
+
+## calculate k (rate constant) PO4
+x$k2 <- -1*(log(1-x$PO4_Retention)/x$tau)
+#plot((x$tau), (x$k2), log = "xy", pch = 16, col = c("darkgreen", "darkgreen", "darkgreen", "blue", "red")[x$Type])
+plot(log(x$tau), log(x$k2), pch = 16, col = c("darkgreen", "darkgreen", "darkgreen", "blue", "red")[x$Type])
+legend("topright", c("Wetland", "Lake", "Reservoir"), pch = 16, col = c("darkgreen", "blue", "red"))
+fit <- lm(log(k2)~log(tau), data = x)
+abline(fit)
+summary(fit)
+
