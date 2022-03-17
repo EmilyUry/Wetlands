704047d91982db233efb7ddbf6d29e854cd71eda
EmilyUry <ury.emily@gmail.com>
2022-03-17 17:41:17 -0400

    began data summary and visualization

--- DU_data_exploration.R
+++ DU_data_exploration.R
@@ -22,20 +22,130 @@ x <- read.csv("P_flux.csv", head = TRUE)
 
 ## data set-up
 
-## merge multiple inflows for OH, KE, DY; and outflows for FE
+## merge multiple inflows and outflows for volume and solutes
+##
+###### inflows = Surface + Tile + Precip
+###### outflows = Outflow + Outflow Leak (FE only)
 
-table(x$Wetland_ID, x$Station)
+flows.combine <- x %>%
+  pivot_wider(names_from = Station, 
+              values_from = c(Flow_volume_m3, TP_kg_day, TDP_kg_day, 
+                              SRP_kg_day, PP_kg_day)) %>%
+  rowwise() %>%
+  mutate(VOL.IN = sum(Flow_volume_m3_Inflow_tile, Flow_volume_m3_Inflow_surface, 
+                          Flow_volume_m3_Precipitation, na.rm = TRUE)) %>%
+  mutate(VOL.OUT = sum(Flow_volume_m3_Outflow, Flow_volume_m3_Outflow_Leak, na.rm = TRUE)) %>%
+  mutate(TP.IN = sum(TP_kg_day_Inflow_tile, TP_kg_day_Inflow_tile,
+                         TP_kg_day_Precipitation, na.rm = TRUE)) %>%
+  mutate(TP.OUT = sum(TP_kg_day_Outflow, TP_kg_day_Outflow_Leak, na.rm = TRUE)) %>%
+  mutate(TDP.IN = sum(TDP_kg_day_Inflow_tile, TDP_kg_day_Inflow_tile,
+                           TDP_kg_day_Precipitation, na.rm = TRUE)) %>%
+  mutate(TDP.OUT = sum(TDP_kg_day_Outflow, TDP_kg_day_Outflow_Leak, na.rm = TRUE)) %>%  
+  mutate(SRP.IN = sum(SRP_kg_day_Inflow_tile, SRP_kg_day_Inflow_tile,
+                           SRP_kg_day_Precipitation, na.rm = TRUE)) %>%
+  mutate(SRP.OUT = sum(SRP_kg_day_Outflow, SRP_kg_day_Outflow_Leak, na.rm = TRUE)) %>%
+  mutate(PP.IN = sum(PP_kg_day_Inflow_tile, PP_kg_day_Inflow_tile,
+                           PP_kg_day_Precipitation, na.rm = TRUE)) %>%
+  mutate(PP.OUT = sum(PP_kg_day_Outflow, PP_kg_day_Outflow_Leak, na.rm = TRUE)) %>%
+  select(Wetland_ID, Date, Water_year, Month, Day, VOL.IN, VOL.OUT, TP.IN, TP.OUT,
+         TDP.IN, TDP.OUT, SRP.IN, SRP.OUT, PP.IN, PP.OUT) 
 
-x$Flow_volume_m3 <- as.numeric(x$Flow_volume_m3)
 
-Tile <- x %>%
-  group_by(Wetland_ID, Water_year) %>%
-  filter(Station == "Inflow_tile") %>%
-  summarise(Tile_Inflow = sum(Flow_volume_m3))
+### calcualte mass removal, percent removal and concentration (mg/L)
+rem.calcs <- flows.combine %>% 
+  rowwise() %>%
+  mutate(flow.atten = sum(VOL.IN, -VOL.OUT, na.rm = TRUE)) %>%
+  mutate(flow.atten.percent = flow.atten/VOL.IN*100) %>%
+  mutate(TP.rem = sum(TP.IN, -TP.OUT, na.rm = TRUE)) %>%
+  mutate(TP.rem.percent = TP.rem/TP.IN*100) %>%
+  mutate(TDP.rem = sum(TDP.IN, -TDP.OUT, na.rm = TRUE)) %>%
+  mutate(TDP.rem.percent = TDP.rem/TDP.IN*100) %>%
+  mutate(SRP.rem = sum(SRP.IN, -SRP.OUT, na.rm = TRUE)) %>%
+  mutate(SRP.rem.percent = SRP.rem/SRP.IN*100) %>%
+  mutate(PP.rem = sum(PP.IN, -PP.OUT, na.rm = TRUE)) %>%
+  mutate(PP.rem.percent = PP.rem/PP.IN*100) %>%
+  mutate(TP.conc.IN = TP.IN/VOL.IN*1000) %>%
+  mutate(TP.conc.OUT = TP.OUT/VOL.OUT*1000) %>%
+  mutate(TDP.conc.IN = TDP.IN/VOL.IN*1000) %>%
+  mutate(TDP.conc.OUT = TDP.OUT/VOL.OUT*1000) %>%
+  mutate(SRP.conc.IN = SRP.IN/VOL.IN*1000) %>%
+  mutate(SRP.conc.OUT = SRP.OUT/VOL.OUT*1000) %>%
+  mutate(PP.conc.IN = PP.IN/VOL.IN*1000) %>%
+  mutate(PP.conc.OUT = PP.OUT/VOL.OUT*1000) 
 
 
 
-## Annual summary
+### Annual summaries
 
 
+
+
+
+### Seasonal summaries
+
+
+
+
+
+### Montly summaries
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#######
+## Junk Code
+
+
+rem.calcs["TP.rem.percent"][rem.calcs["TP.rem.percent"] == "-Inf"] <- -99
+rem.calcs["flow.atten.percent"][rem.calcs["flow.atten.percent"] == "-Inf"] <- -99
+
+test <- rem.calcs[which(rem.calcs$flow.atten.percent == "NaN"),]
+
+hist <- ggplot(rem.calcs, aes(TP.rem.percent)) +
+  geom_histogram(bins = 50) +
+  xlim(-100,100) +
+  facet_wrap(~Wetland_ID)
+
+hist <- ggplot(rem.calcs, aes(flow.atten.percent)) +
+  geom_histogram(bins = 50) +
+  xlim(-100,100) +
+  facet_wrap(~Wetland_ID)
+
+hist
+
+
+hist <- ggplot(rem.calcs, aes(PP.conc.IN)) +
+  geom_histogram(bins = 50) +
+  xlim(0,0.2) +
+  facet_wrap(~Wetland_ID)
+hist
+
+ggplot(rem.calcs, aes(x = Wetland_ID, y = PP.conc.IN)) +
+  geom_boxplot()
+
+ggplot(rem.calcs, aes(x = Wetland_ID, y = PP.IN)) +
+  geom_boxplot() +
+  ylim(0,0.05)
 
