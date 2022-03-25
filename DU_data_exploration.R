11e3cc19523dd9d090d6c1f88ea87f1f21659bf6
EmilyUry <ury.emily@gmail.com>
2022-03-25 16:49:40 -0400

    redoing annual summaries

--- DU_data_exploration.R
+++ DU_data_exploration.R
@@ -15,12 +15,14 @@ setwd("C:/Users/Emily Ury/OneDrive - University of Waterloo/Wetlands_local/Data_
 library(ggplot2)
 library(tidyverse)
 library(viridis)
+library(gridExtra)
 
 x <- read.csv("P_flux.csv", head = TRUE)
 
 #######################
 
 ## data set-up
+x$Water_year <- as.factor(x$Water_year)
 
 ## merge multiple inflows and outflows for volume and solutes
 ##
@@ -50,6 +52,72 @@ flows.combine <- x %>%
   mutate(PP.OUT = sum(PP_kg_day_Outflow, PP_kg_day_Outflow_Leak, na.rm = TRUE)) %>%
   select(Wetland_ID, Date, Water_year, Month, Day, Precip, VOL.IN, VOL.OUT, TP.IN, TP.OUT,
          TDP.IN, TDP.OUT, SRP.IN, SRP.OUT, PP.IN, PP.OUT) 
+
+
+######################
+#                    #
+##  annual summary  ##
+#                    #
+######################
+
+annual.summary <- flows.combine %>%
+  group_by(Wetland_ID, Water_year) %>%
+  summarise(across(where(is.numeric), sum, na.rm = TRUE))
+
+### calcualte mass removal, percent removal and concentration (mg/L)
+
+annual.summary <- annual.summary %>%
+  rowwise() %>%
+  mutate(flow.atten = sum(VOL.IN, -VOL.OUT, na.rm = TRUE)) %>%
+  mutate(flow.atten.percent = flow.atten/VOL.IN*100) %>%
+  mutate(TP.rem = sum(TP.IN, -TP.OUT, na.rm = TRUE)) %>%
+  mutate(TP.rem.percent = TP.rem/TP.IN*100) %>%
+  mutate(TDP.rem = sum(TDP.IN, -TDP.OUT, na.rm = TRUE)) %>%
+  mutate(TDP.rem.percent = TDP.rem/TDP.IN*100) %>%
+  mutate(SRP.rem = sum(SRP.IN, -SRP.OUT, na.rm = TRUE)) %>%
+  mutate(SRP.rem.percent = SRP.rem/SRP.IN*100) %>%
+  mutate(PP.rem = sum(PP.IN, -PP.OUT, na.rm = TRUE)) %>%
+  mutate(PP.rem.percent = PP.rem/PP.IN*100) %>%
+  mutate(TP.conc.IN = TP.IN/VOL.IN*1000) %>%
+  mutate(TP.conc.OUT = TP.OUT/VOL.OUT*1000) %>%
+  mutate(TDP.conc.IN = TDP.IN/VOL.IN*1000) %>%
+  mutate(TDP.conc.OUT = TDP.OUT/VOL.OUT*1000) %>%
+  mutate(SRP.conc.IN = SRP.IN/VOL.IN*1000) %>%
+  mutate(SRP.conc.OUT = SRP.OUT/VOL.OUT*1000) %>%
+  mutate(PP.conc.IN = PP.IN/VOL.IN*1000) %>%
+  mutate(PP.conc.OUT = PP.OUT/VOL.OUT*1000) 
+
+#### add site info
+aux.d <- read.csv("Wetland_Info.csv", head = TRUE)
+names(aux.d)[1] <- "Wetland_ID"
+names(aux.d) <- c("Wetland_ID", "Name", "Established", "Area", "Volume", "CA",
+                  "CA_WA", "Crop18", "Crop19", "Crop20", "Crop21", "SOM", "SBP", "UBP")
+annual.summary <- annual.summary  %>%
+  left_join(aux.d, by = "Wetland_ID")
+
+
+
+### plot data
+
+annual.summary[annual.summary == -Inf] <- NaN
+annual.summary[annual.summary == Inf] <- NaN
+
+data <- annual.summary
+
+plot(data$Precip, data$TP.rem)
+
+ggplot(data, aes(fill = Wetland_ID, y = TP.rem/Area, x = Water_year)) +
+  geom_bar(position = "dodge", stat = "identity") + 
+  ylab("TP retention (kg/ha)")
+
+
+
+
+
+
+
+
+
 
 
 ### calcualte mass removal, percent removal and concentration (mg/L)
@@ -126,7 +194,7 @@ monthly.summary <- rem.calcs %>%
 
 
 #### data visualization
-library(gridExtra)
+
 
 OH <- monthly.summary[which(monthly.summary$Wetland_ID == "OH"),]
 plot(OH$Month, OH$TP.IN)
