aa97795a3d66f3c81cbe8d6adb52301607407c04
EmilyUry <ury.emily@gmail.com>
2022-03-30 11:51:22 -0400

    cleaning code up moved annual and monthly summaries to new scripts

--- DU_data_exploration.R
+++ DU_data_exploration.R
@@ -143,38 +143,6 @@ summary(fit)
 
 
 
-######################
-#                    #
-##  annual summary  ##
-#                    #
-######################
-
-annual.summary <- flows.combine %>%
-  group_by(Wetland_ID, Water_year) %>%
-  summarise(across(where(is.numeric), sum, na.rm = TRUE))
-
-### calcualte mass removal, percent removal and concentration (mg/L)
-
-annual.summary <- annual.summary %>%
-  rowwise() %>%
-  mutate(flow.atten = sum(VOL.IN, -VOL.OUT, na.rm = TRUE)) %>%
-  mutate(flow.atten.percent = flow.atten/VOL.IN*100) %>%
-  mutate(TP.rem = sum(TP.IN, -TP.OUT, na.rm = TRUE)) %>%
-  mutate(TP.rem.percent = TP.rem/TP.IN*100) %>%
-  mutate(TDP.rem = sum(TDP.IN, -TDP.OUT, na.rm = TRUE)) %>%
-  mutate(TDP.rem.percent = TDP.rem/TDP.IN*100) %>%
-  mutate(SRP.rem = sum(SRP.IN, -SRP.OUT, na.rm = TRUE)) %>%
-  mutate(SRP.rem.percent = SRP.rem/SRP.IN*100) %>%
-  mutate(PP.rem = sum(PP.IN, -PP.OUT, na.rm = TRUE)) %>%
-  mutate(PP.rem.percent = PP.rem/PP.IN*100) %>%
-  mutate(TP.conc.IN = TP.IN/VOL.IN*1000) %>%
-  mutate(TP.conc.OUT = TP.OUT/VOL.OUT*1000) %>%
-  mutate(TDP.conc.IN = TDP.IN/VOL.IN*1000) %>%
-  mutate(TDP.conc.OUT = TDP.OUT/VOL.OUT*1000) %>%
-  mutate(SRP.conc.IN = SRP.IN/VOL.IN*1000) %>%
-  mutate(SRP.conc.OUT = SRP.OUT/VOL.OUT*1000) %>%
-  mutate(PP.conc.IN = PP.IN/VOL.IN*1000) %>%
-  mutate(PP.conc.OUT = PP.OUT/VOL.OUT*1000) 
 
 #### add site info
 aux.d <- read.csv("Wetland_Info.csv", head = TRUE)
@@ -183,48 +151,6 @@ names(aux.d) <- c("Wetland_ID", "Name", "Established", "Area", "Volume", "CA",
                   "CA_WA", "Crop18", "Crop19", "Crop20", "Crop21", "SOM", "SBP", "UBP")
 annual.summary <- annual.summary  %>%
   left_join(aux.d, by = "Wetland_ID")
-
-
-
-### plot data
-
-annual.summary[annual.summary == -Inf] <- NaN
-annual.summary[annual.summary == Inf] <- NaN
-
-data <- annual.summary
-
-ggplot(data, aes(fill = Wetland_ID, y = TP.rem/Area, x = Water_year)) +
-  geom_bar(position = "dodge", stat = "identity") + 
-  ylab("TP retention (kg/ha)")
-
-ggplot(data, aes(fill = Wetland_ID, y = SRP.rem/Area, x = Water_year)) +
-  geom_bar(position = "dodge", stat = "identity") + 
-  ylab("SRP retention (kg/ha)")
-
-
-
-plot(data$Area, data$SRP.rem, pch = 16,  cex = 2,
-     col = c("#F8766D", "#CD9600", "#7CAE00", "#00BE67",
-             "#00BFC4", "#00A9FF", "#C77CFF", "#FF61CC")[data$Wetland_ID],
-     ylab = "SRP retention (kg/year",
-     xlab = "Wetland area (ha)")
-legend("topright", c("BL", "DY", "FE", "KE", "LL", "MA", "MO", "OH"),
-       pch = 16, pt.cex = 2,  ncol = 2,
-       col = c("#F8766D", "#CD9600", "#7CAE00", "#00BE67",
-               "#00BFC4", "#00A9FF", "#C77CFF", "#FF61CC"))
-
-plot(data$Area, data$TP.rem, pch = 16,  cex = 2,
-     col = c("#F8766D", "#CD9600", "#7CAE00", "#00BE67",
-             "#00BFC4", "#00A9FF", "#C77CFF", "#FF61CC")[data$Wetland_ID],
-     ylab = "TP retention (kg/year",
-     xlab = "Wetland area (ha)")
-legend("bottomright", c("BL", "DY", "FE", "KE", "LL", "MA", "MO", "OH"),
-       pch = 16, pt.cex = 2,  ncol = 2,
-       col = c("#F8766D", "#CD9600", "#7CAE00", "#00BE67",
-               "#00BFC4", "#00A9FF", "#C77CFF", "#FF61CC"))
-
-
-
 
 
 
